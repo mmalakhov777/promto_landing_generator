546e1406d8f9101bf199701bc96ce9c492f1fd12
User <user@promto.ai>
2026-03-27 22:30:55 +0000

    Добавить счётчик Яндекс.Метрики (ID 108237139)
    Webvisor, clickmap, ecommerce, accurateTrackBounce включены.
    
    Made with promto.ai (user: 321097981)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -145,6 +145,17 @@ export default function RootLayout({
     <html lang="ru" className={`${onest.variable} antialiased`} suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+        {/* Yandex.Metrika counter */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=108237139','ym');ym(108237139,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});`,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/108237139" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
       </head>
       <body className="min-h-dvh flex flex-col bg-bg-page text-text-primary transition-colors duration-300">
         <script
